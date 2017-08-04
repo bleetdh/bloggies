6b62a3a80abf20eafe13eed90e7bd873370f45b3
Prima Aulia Gusta <primaulia@gmail.com>
2017-08-04 12:37:32 +0800

    latest code

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'application#home' # don't need to specify path
   get '/about', to: 'static#about'
 
-  get '/:username', to: 'user#show'
+  # method to create a restful routes
+  resources :users
 
-  # router.get('/about', applicationCtrl.getAbout)
+  # resources :posts
 end
