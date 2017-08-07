f0e08def8ddbd41657f920ef6dd071fa11c2eb3a
Prima Aulia Gusta <primaulia@gmail.com>
2017-08-07 11:51:01 +0800

    create routes with form

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -12,4 +12,24 @@ class StaticPageController < ApplicationController
     @current_post = Post.find(params[:id])
     render :show
   end
+
+  def new
+  end
+
+  def create_post
+    # rails way to secure mass assigning
+    Post.create(params.require(:post).permit(:title, :content))
+
+    redirect_to root_path
+
+    # same thing but individually
+    # @new_post = Post.new
+    #
+    # @new_post.title = params[:post][:title]
+    # @new_post.content = params[:post][:content]
+
+    # render json: params
+    # ? what happened after creating post
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,20 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static_page#home' # don't need to specify path
-  get '/about', to: 'static#about'
+
+  # unused routes... YET
+  # get '/about', to: 'static#about'
+  # get '/users/:username', to: 'static_page#show_user'
 
   # rails g controller created
   # get 'static_page/home' # GET req to /static_page/home
-
   get '/list', to: 'static_page#list'
+
+  get '/posts/new', to: 'static_page#new'
+  post '/posts/new', to: 'static_page#create_post'
+  # start of a restful routes
   get '/posts/:id', to: 'static_page#show_post', as: 'single_blog'
 
-  get '/users/:username', to: 'static_page#show_user'
 
   # method to create a restful routes
   # resources :posts
